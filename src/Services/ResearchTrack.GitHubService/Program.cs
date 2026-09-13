6d6850caff50f61fc2a389f96aa6b59456443d3c
pamudiab <pamudiab@gmail.com>
2026-09-13 20:31:28 +0530

    feat(observability): expose Prometheus metrics endpoint on backend services

--- src/Services/ResearchTrack.GitHubService/Program.cs
+++ src/Services/ResearchTrack.GitHubService/Program.cs
@@ -1,3 +1,4 @@
+using Prometheus;
 using ResearchTrack.BuildingBlocks.Api.Extensions;
 using ResearchTrack.BuildingBlocks.Api.Security;
 using ResearchTrack.GitHubService.Extensions;
@@ -12,7 +13,9 @@ builder.Services.AddGitHubPersistence(builder.Configuration);
 builder.Services.AddGitHubFeatures(builder.Configuration);
 
 var app = builder.Build();
+app.UseHttpMetrics();
 app.UseResearchTrackApi();
+app.MapMetrics();
 app.Run();
 
 public partial class Program;
